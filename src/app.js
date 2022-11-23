044308cb766310bee4083acf3cedc90db286db4c
Lucas M. Pedrozo P <93496498+lpedrozop@users.noreply.github.com>
2022-11-23 14:24:15 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from 'express'
 import session from 'express-session'
 import userRouter from "./routes/user.router.js";
 import cors from "cors"
+import morgan from "morgan";
 
 const app = express()
 app.use(express.json())
+app.use(morgan("common"))
 app.use(cors());
 
 app.use(session({
@@ -14,7 +16,7 @@ app.use(session({
 }))
 app.use(userRouter)
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).json({
         message: 'Endpoint Not Found'
     })
